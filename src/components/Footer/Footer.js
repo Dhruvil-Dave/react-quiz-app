2787fb4113e90fbe2eff9cdf3c872f372e081254
Dhruvil-Dave <dhruvildave45@gmail.com>
2021-05-29 01:42:29 +0530

    initial

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://github.com/Dhruvil-Dave"
+          style={{ cursor: "pointer" }}
+        >
+          Dhruvil Dave
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
